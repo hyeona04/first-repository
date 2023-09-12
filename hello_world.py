3506d0788c763842373a37020b459a57be1b8c4b
hyeona04 <hannah040206@naver.com>
2023-09-12 14:27:50 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,10 @@
-def print_hello():
-    print("Hello World!!")
-if __name__ == '__main__':
-    print_hello()
 
+
+def fibonacci(n):
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        next_num = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_num)
+    return fib_sequence
+
+print(fibonacci(10))  # Print the first 10 Fibonacci numbers
